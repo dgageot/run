3b91025bc75eed9d18d3516a8d150dac882d0d03
David Gageot <david@gageot.net>
2017-06-19 17:01:25 +0200

    First command is always `docker`
    Signed-off-by: David Gageot <david@gageot.net>

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 	command := expand(os.Args[1:], aliases)
 
-	cmd := exec.Command(command[0], command[1:]...)
+	cmd := exec.Command("docker", command...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
